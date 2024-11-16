e92317e9da8c7f85e7d654fe07a5d7f37e1ae19f
Hamza Yacine <hamzayacine0711@gmail.com>
2024-11-16 19:42:27 +0100

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './graphql/models/User';
+import { UserSetting } from './graphql/models/UserSetting'
+import { UsersModule } from './users/user.module'
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123',
+      database:'postgresgraphql',
+      entities: [User, UserSetting],
+      synchronize: true,
+      logging: false,
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
