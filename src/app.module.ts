428e8879dfe5fcf6719ef930356fbbf6755b3058
Hamza Yacine <hamzayacine0711@gmail.com>
2024-11-17 13:03:57 +0100

    configure with env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,39 @@
 import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule ,TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from './graphql/models/User';
 import { UserSetting } from './graphql/models/UserSetting'
 import { UsersModule } from './users/user.module'
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/schema.gql',
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123',
-      database:'postgresgraphql',
-      entities: [User, UserSetting],
-      synchronize: true,
-      logging: false,
+   
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
+        type: configService.get<any>('DB_TYPE'),
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [User, UserSetting],  
+        synchronize: configService.get<boolean>('DB_SYNCHRONIZE'),
+        logging: configService.get<boolean>('DB_LOGGING'),
+      }),
     }),
+
+
+
     UsersModule,
   ],
   controllers: [],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);  
+  const port = configService.get<number>('PORT', 3000);  
+  await app.listen(port);  // Démarre le serveur sur le port récupéré
+
+  console.log(`App is running on port: ${port}`);
 }
+
 bootstrap();
